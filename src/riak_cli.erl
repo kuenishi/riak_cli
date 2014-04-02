3809937df6bdb9057ef55b24eb4a09cd528cad41
UENISHI Kota <kota@basho.com>
2014-04-02 12:00:19 +0900

    first commit to escriptize

---
+++ src/riak_cli.erl
@@ -0,0 +1,15 @@
+%%%-------------------------------------------------------------------
+%%% @author UENISHI Kota <kota@basho.com>
+%%% @copyright (C) 2014, UENISHI Kota
+%%% @doc
+%%%
+%%% @end
+%%% Created :  2 Apr 2014 by UENISHI Kota <kota@basho.com>
+%%%-------------------------------------------------------------------
+-module(riak_cli).
+
+-export([main/1]).
+
+main([]) ->
+    io:format("hello"),
+    ok.
